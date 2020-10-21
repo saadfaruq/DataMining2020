d43904a52d06b9bc30fdb4be39c56709ea54fbef
saadfaruq <33988013+saadfaruq@users.noreply.github.com>
2020-10-21 21:30:26 +0100

    Add files via upload

--- week1/Assignment1_solution.py
+++ week1/Assignment1_solution.py
@@ -1,17 +1,19 @@
 # creating a new class name ListKeeper
 class ListKeeper():
-    
-    def __init__(self):
-        self.named_lists = {
-            'example':[1,2,3,4,5]
-        }       
+    def _init_(self):
+        self.stored_lists = {'example':[1,2,3,4,5]}
+        
     def show(self):                                                  #defining a function to show all list name 
-        return list(self.named_lists.keys())
+        return list(self.stored_lists.keys())
+    
     def add(self, name, list_elements):                                # defining a funtion add
-        self.named_lists[name] = list_elements    
+        self.stored_lists[name] = list_elements
+        
     def delete(self, name):                                             # defining a funtion to delete 
-        del self.named_lists[name]
+        del self.stored_lists[name]
+    
     def sort(self, name):                                              # defining a funtion to sort
-        return sorted(self.named_lists[name]) 
+        return sorted(self.stored_lists[name])
+    
     def append(self, name, list_elements):                             # defining a funtion to append
-        self.named_lists[name].extend(list_elements)
+        self.stored_lists[name].extend(list_elements)
